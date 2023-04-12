0f45a84efadb38e5bc8e2e4f4daeb4ea26e98e1f
MarynaShchedrova <shchedrovamaryna@gmail.com>
2023-04-12 16:37:03 +0300

    update

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const input = document.querySelector('.search-form-input');
-const btnSearch = document.querySelector('.search-form');
+const btnSearch = document.querySelector('.search-form-button');
 const gallery = document.querySelector('.gallery');
 const btnLoadMore = document.querySelector('.load-more');
 let gallerySimpleLightbox = new SimpleLightbox('.gallery a');
