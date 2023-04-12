3138ccd54e6cc265edec238e28acbe08c763f671
MarynaShchedrova <shchedrovamaryna@gmail.com>
2023-04-12 16:44:55 +0300

    push

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import { fetchImages } from '../js/fetchImages';
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
@@ -20,12 +20,12 @@ btnSearch.addEventListener('click', event => {
     if (trimmedValue !== '') {
         fetchImages(trimmedValue, pageNumber).then(foundData => {
             if (foundData.hits.length === 0) {
-                Notiflix.Notify.failure(
+                Notify.failure(
                     'Sorry, there are no images matching your search query. Please try again.'
                 );
             } else {
                 renderImageList(foundData.hits);
-                Notiflix.Notify.success(
+                Notify.success(
                     `Hooray! We found ${foundData.totalHits} images.`
                 );
                 btnLoadMore.style.display = 'block';
@@ -41,12 +41,12 @@ btnLoadMore.addEventListener('click', () => {
     btnLoadMore.style.display = 'none';
     fetchImages(trimmedValue, pageNumber).then(foundData => {
         if (foundData.hits.length === 0) {
-            Notiflix.Notify.failure(
+            Notify.failure(
                 'Sorry, there are no images matching your search query. Please try again.'
             );
         } else {
             renderImageList(foundData.hits);
-            Notiflix.Notify.success(
+            Notify.success(
                 `Hooray! We found ${foundData.totalHits} images.`
             );
             btnLoadMore.style.display = 'block';
